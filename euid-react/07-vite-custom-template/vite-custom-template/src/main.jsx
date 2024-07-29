e2ca7f3b448bcff528dfe288b1ceaca24e6db2e5
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-29 13:05:05 +0900

    Vite custom template

---
+++ euid-react/07-vite-custom-template/vite-custom-template/src/main.jsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import ReactDom, { createRoot } from "react-dom/client";
+
+//Vite 클라이언트 환경(브라우저)에서 사용 가능한 환경 변수
+// console.log(import.meta.env);
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+const domNode = document.getElementById("react-app");
+createRoot(domNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+console.log(React.version, ReactDom);
