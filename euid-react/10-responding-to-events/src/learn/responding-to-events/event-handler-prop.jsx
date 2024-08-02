bd1f90ee80361e0548dc2c9b337152fe563b7c95
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-08-02 15:33:00 +0900

    이벤트 전파 III - 기본 작동 방지

--- euid-react/10-responding-to-events/src/learn/responding-to-events/event-handler-prop.jsx
+++ euid-react/10-responding-to-events/src/learn/responding-to-events/event-handler-prop.jsx
@@ -1,30 +1,73 @@
-import { func } from 'prop-types';
+// --------------------------------------------------------------------------
+// ✅ 이벤트 전파
+// --------------------------------------------------------------------------
+// - [x] 전파 중지 (개별적으로 할 수 있음)
+// - [x] 전파 대안으로 핸들러 전달
+// - [x] 기본 작동 방지
+// --------------------------------------------------------------------------
 
-EventHandlerProp.propTypes = {
-  onPrintMessage: func,
-};
+import LayoutBox from './LayoutBox';
 
-// 하위 컴포넌트
-function EventHandlerProp({ onPrintMessage }) {
-  const handleEnter = () => {
-    console.log('enter');
-    onPrintMessage?.(' ⭐️');
-  };
+// RGB 모드
+// CMYK 모드
 
-  const handleLeave = () => console.log('leave');
+// event.preventDefault(); // 이벤트 기본 작동을 차단
 
+// Event Delegation (위임)
+// Event Propagation (전파)
+// event.stopPropagation(); // 이벤트 전파 중지
+
+function EventPropagation() {
   return (
     <details>
-      <summary onPointerEnter={handleEnter} onPointerLeave={handleLeave}>
-        <b>이벤트 핸들러 추가하기</b>
+      <summary>
+        <b>이벤트 전파 &amp; 기본 작동 방지</b>
       </summary>
-      <p>
-        이벤트 핸들러 추가를 위해서는 먼저 함수를 정의하고
-        <br />
-        이를 적절한 JSX 요소에 prop으로 전달해야 합니다.
-      </p>
+      {/* 상위 컴포넌트 : 정민 */}
+      <LayoutBox
+        style={styles.cyan}
+        onClick={(e) => {
+          console.log('cyan', e.target);
+        }}
+      >
+        <LayoutBox
+          style={styles.magenta}
+          onClick={(e) => {
+            console.log('magenta', e.target);
+          }}
+        >
+          <LayoutBox
+            style={styles.yellow}
+            onClick={(e) => {
+              console.log('yellow', e.target);
+            }}
+          >
+            <LayoutBox
+              style={styles.purple}
+              onClick={(e) => {
+                console.log('purple', e.target);
+              }}
+            ></LayoutBox>
+          </LayoutBox>
+        </LayoutBox>
+      </LayoutBox>
     </details>
   );
 }
 
-export default EventHandlerProp;
+const styles = {
+  cyan: {
+    '--color': 'var(--cyan)',
+  },
+  magenta: {
+    '--color': 'var(--magenta)',
+  },
+  yellow: {
+    '--color': 'var(--yellow)',
+  },
+  purple: {
+    '--color': 'var(--purple, #7423f6)',
+  },
+};
+
+export default EventPropagation;
