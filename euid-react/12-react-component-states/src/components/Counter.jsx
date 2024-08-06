d54c18d04a0619be005ddcc8a6a1459f313878db
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-08-06 14:04:15 +0900

    리액트 상태 업데이트에 대한 이해 - 상태 업데이트 2가지 방법 - 상태 업데이트는 큐 자료 구조 활용

--- euid-react/12-react-component-states/src/components/Counter.jsx
+++ euid-react/12-react-component-states/src/components/Counter.jsx
@@ -51,7 +51,7 @@ function Counter({ count: initialCount = 1, step = 1, min = 1, max = 1000 }) {
 
   const handleDecrease = () => {
     // ----------------------------------------------------------------------
-    console.group('지역 변수');
+    console.groupCollapsed('지역 변수');
     console.log(`[-] number = ${number}`);
     number -= 1;
     // 함수 내부의 지역 변수는 즉시 값이 변경 됨!
@@ -60,10 +60,20 @@ function Counter({ count: initialCount = 1, step = 1, min = 1, max = 1000 }) {
     // ----------------------------------------------------------------------
 
     // ----------------------------------------------------------------------
-    console.group('컴포넌트 상태');
+    console.groupCollapsed('컴포넌트 상태');
     console.log(`[-] count = ${count}`);
     const nextCount = count - step;
     setCount(nextCount);
+
+    // 클래스 컴포넌트
+    // this.setState(nextState, callback?)
+
+    // 함수 컴포넌트
+    // const [state, setState] = useState() (상태 관리)
+    // setState(nextState)
+    //     +
+    // useEffect (이펙트 관리)
+
     // 컴포넌트의 상태는 리액트가 관리 (JavaScript가 아니라)
     // - 동일 입력, 동일 출력 조건이 충족되면 순수 함수이다.
     // - 상태는 불변 데이터(Immutable Data)로 관리된다.
@@ -75,7 +85,7 @@ function Counter({ count: initialCount = 1, step = 1, min = 1, max = 1000 }) {
 
   const handleIncrease = () => {
     // ----------------------------------------------------------------------
-    console.group('지역 변수');
+    console.groupCollapsed('지역 변수');
     console.log(`[+] number = ${number}`);
     number += 1;
     // 함수 내부의 지역 변수는 즉시 값이 변경 됨!
@@ -84,13 +94,49 @@ function Counter({ count: initialCount = 1, step = 1, min = 1, max = 1000 }) {
     // ----------------------------------------------------------------------
 
     // ----------------------------------------------------------------------
-    console.group('컴포넌트 상태');
-    console.log(`[+] count = ${count}`);
-    const nextCount = count + step;
-    setCount(nextCount);
+    // console.groupCollapsed('컴포넌트 상태');
+    // console.log(`[+] count = ${count}`);
+    // const nextCount = count + step;
+    // setCount(nextCount);
     // 컴포넌트의 상태는 즉시 값이 변경되지 않음!
+    // console.log(`[+] count = ${count}`);
+    // console.groupEnd('컴포넌트 상태');
+    // ----------------------------------------------------------------------
+
+    console.group('컴포넌트 상태');
+    console.log(`[+] count += ${step * 3}`);
+
+    // -------------------------------------------
+    // setState((prevState) => nextState) API
+    // React State Queue [updateState1, updateState2, updateState3]
+    // const nextState = updateState3(updateState2(updateState1()))
+    // -------------------------------------------
+    // setCount((prevCount) => prevCount + step); // `3` + 1 = 4
+    // setCount((prevCount) => prevCount + step); // `4` + 1 = 5
+    // setCount((prevCount) => prevCount + step); // `5` + 1 = 6
+
+    Array(6)
+      .fill(null)
+      .forEach(() => {
+        setCount((c) => c + step);
+      });
+
+    // -------------------------------------------
+    // setState(nextState) API
+    // -------------------------------------------
+    // setCount(count + step); // `3` + 1 = 4
+    // setCount(count + step); // `3` + 1 = 4
+    // setCount(count + step); // `3` + 1 = 4
+
+    // Array(3)
+    //   .fill(null)
+    //   .forEach(() => {
+    //     setCount(count + step);
+    //   });
+
     console.log(`[+] count = ${count}`);
     console.groupEnd('컴포넌트 상태');
+
     // ----------------------------------------------------------------------
   };
 
