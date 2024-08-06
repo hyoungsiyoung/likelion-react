a466551f47c181183c177c56984691ee32d94719
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-08-06 10:37:11 +0900

    카운터 선언적 제어 - 함수 컴포넌트 - 훅 함수의 규칙 - 상태 선언, 이벤트 핸들러, 파생된 상태 활용

--- euid-react/12-react-component-states/src/styles/Playground.jsx
+++ euid-react/12-react-component-states/src/styles/Playground.jsx
@@ -1,9 +1,10 @@
-import TermAndConditions from '@/components/TermAndCondition';
+import Counter from '@/components/Counter';
 
 function Playground() {
   return (
     <>
-      <TermAndConditions />
+      <Counter count={3} min={2} max={6} />
+      {/* <Counter count={10} step={4} min={20} max={40} /> */}
     </>
   );
 }
