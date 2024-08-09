c9df7fbb032dd0ba119fb61b99283f013a2b289d
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-08-09 15:04:11 +0900

    틱택토 게임 만들기 - 게임 승리 조건 - 게임 승리자가 있는 지 확인하는 함수 작성 중...

--- euid-react/13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
+++ euid-react/13-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -4,10 +4,17 @@
 // - [x] squares 배열 데이터를 게임의 상수로 설정합니다.
 // - [x] squares 배열 데이터의 초기 상태 값은 9개의 `null`로 구성합니다.
 // - [x] squares 배열 데이터 모듈을 불러온 후, 순환해 Sqaure 컴포넌트를 리스트 렌더링 합니다.
+// - [x] 게임 진행을 처리하는 함수 로직을 작성하고, 리액트에게 다음 상태 변경에 대해 말해주세요.
+// - [ ] 게임이 이겼는 지, 졌는 지 확인하는 승리 조건을 게임의 상수로 선언합니다.
 // --------------------------------------------------------------------------
 
 import { useState } from 'react';
-import { INITIAL_SQUARES, PLAYER, PLAYER_COUNT } from '@/tic-tac-toe/constants';
+import {
+  INITIAL_SQUARES,
+  PLAYER,
+  PLAYER_COUNT,
+  checkeWinner,
+} from '@/tic-tac-toe/constants';
 import S from './Squares.module.css';
 import Square from '../Square/Square';
 
@@ -21,23 +28,20 @@ function Squares() {
   // [게임 상태 업데이트 기능] ----------------------------------------------------
 
   // 게임을 진행하는 함수
-  const playGame = (index) => {
-    // 게임 상태 변경 -> 리액트에게 렌더 트리거(요청) -> 리액트는 컴포넌트 다시 렌더링 -> 렌더 트리
-    // -> 리액트 돔 이전 렌더 트리 현재 렌더 트리 비교 -> 차이가 발견 -> 실제 DOM 커밋(변경된 부분만 패치)
-    // console.log(`play game #${index}`);
+  const handlePlayGame = (index) => () => {
+    // 게임이 끝났는가? 아니면 아직 진행 중인가?
+    // 게임이 끝났다면? 게임이 끝났음을 사용자에게 고하게!
+    // 반환 값에 따라 게임을 진행할 지, 아닐 지 결정
+    // squares?, isPlayerOneTurn?, gameIndex?, currentPlayer?
+    const result = checkeWinner('????');
+    console.log({ result });
+
+    // 아직 진행 중이라면? 게임 진행 (리액트에게 렌더 요청 -> 화면 변경)
     setSquares((prevSquares) => {
-      // 다음 번 컴포넌트 렌더링 시, 전달(계산)된 현재 시점의 상태: 이전 스퀘어 집합을 순환해서
-      const nextSquares = prevSquares.map((square, squareIndex) => {
-        // 개별 스퀘어의 인덱스와 사용자 행동에 따라 선택된 인덱스를 비교한다.
-        // 만약 그 값이 동일하다면?
-        if (squareIndex === index) {
-          return currentPlayer;
-        }
-        // 동일하지 않은 경우 그냥 이전 값을 반환한다.
-        return square;
+      const nextSquares = prevSquares.map((square, idx) => {
+        return idx === index ? currentPlayer : square;
       });
 
-      // 반환한 값이 다음 번 렌더링에서의 (스냅샷) 상태 값
       return nextSquares;
     });
   };
@@ -45,22 +49,20 @@ function Squares() {
   // [게임 파생된 상태] ----------------------------------------------------------
 
   // 게임 순서 (0, 1, 2, 3, ...)
-  const gameIndex = squares.filter(Boolean).length;
+  const gameIndex = squares.filter(Boolean).length; // 0
 
   // 현재 게임 플레이어 ([0] PLAYER.ONE ↔ [1] PLAYER.TWO)
   // 첫번째 플레이어의 턴인가요?
-  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0;
+  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0; // true
   // 첫번째 플레이어의 턴이면 PLAYER.ONE 아니면 PLAYER.TWO
-  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO;
-
-  console.log('렌더링\n\n', { squares, gameIndex, currentPlayer });
+  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO; // '🍟'
 
   return (
     <div className={S.component}>
       {/* 리액트 (JSX) 마크업 : 리스트 렌더링 */}
       {squares.map((square, index) => {
         return (
-          <Square key={index} onPlay={() => playGame(index)}>
+          <Square key={index} onPlay={handlePlayGame(index)}>
             {square}
           </Square>
         );
--- euid-react/13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
+++ euid-react/13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
@@ -1,5 +1,14 @@
+import S from './Status.module.css';
+
+{
+  /* <h2>승자! : 🟨</h2> */
+}
+{
+  /* <h2>비겼어요... 😳</h2> */
+}
+
 function Status() {
-  return <></>;
+  return <h2 className={S.component}>플레이어 : 🟨</h2>;
 }
 
 export default Status;
