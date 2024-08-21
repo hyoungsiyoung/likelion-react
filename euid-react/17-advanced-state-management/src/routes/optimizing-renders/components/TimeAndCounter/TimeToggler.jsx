3bbac03d57b6c4a823bca40629f67226b12ec74a
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-08-21 11:04:56 +0900

    컴포넌트 렌더링 트리거 - 상태가 변경될 때 - 속성이 변경될 때 - 가설 증명 (함수를 전달할 경우 기억해야 함)

---
+++ euid-react/17-advanced-state-management/src/routes/optimizing-renders/components/TimeAndCounter/TimeToggler.jsx
@@ -0,0 +1,19 @@
+import { func, node } from 'prop-types';
+import S from './style.module.css';
+import useRenderCountLog from '@/hooks/useRenderCountLog';
+
+TimeToggler.propTypes = {
+  children: node.isRequired,
+  onToggle: func,
+};
+
+function TimeToggler({ children, onToggle }) {
+  useRenderCountLog('TimeToggler', '#f97172', 600, 18);
+  return (
+    <button type="button" className={S.button} onClick={onToggle}>
+      {children}
+    </button>
+  );
+}
+
+export default TimeToggler;
